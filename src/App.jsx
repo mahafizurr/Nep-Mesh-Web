54536fe89bd5f77241a4d066f7a18609196595f4
mahafizurr <mahafizurgub@gmail.com>
2024-01-22 21:32:55 +0600

    commit first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className=" bg-slate-800 text-white text-3xl text-center font-bold m-4 p-4">
+          Neptune-Mesh
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
